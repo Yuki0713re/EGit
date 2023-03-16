ed673b36521ffd3b32f9eac4af42f6133971881f
Yuki0713re <117838103+Yuki0713re@users.noreply.github.com>
2023-03-16 11:36:09 +0900

    課題1.29

---
+++ curriculum/src/prefectures/Prefectures.java
@@ -0,0 +1,34 @@
+package prefectures;
+
+
+
+public class Prefectures {
+    private String name;
+    private String capital;
+    private double area;
+    private int index;
+
+    public Prefectures(String name, String capital, double area, int index) {
+        this.name = name;
+        this.capital = capital;
+        this.area = area;
+        this.index = index;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCapital() {
+        return capital;
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+	public int getIndex() {
+		return index;
+	}
+    
+}
